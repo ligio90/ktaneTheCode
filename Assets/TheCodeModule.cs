46b1065d4e77070d74f2d3d8b0af7e00425a8430
Timwi <github@timwi.de>
2018-05-10 23:11:41 +0200

    Add TP support, clean up code, remove unused material files

--- Assets/TheCodeModule.cs
+++ Assets/TheCodeModule.cs
@@ -1,19 +1,15 @@
-﻿using UnityEngine;
-using Rnd = UnityEngine.Random;
-using TheCode;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
-using Newtonsoft.Json;
-using System;
-using System.Collections;
+using System.Text.RegularExpressions;
+using TheCode;
+using UnityEngine;
 
+using Rnd = UnityEngine.Random;
 
-//The Code Module Created by ligio90 and Marksam CREATED BY THEM REALLY ©2018 THEM GIVE THEM ALL YO MONEY
-//The Code Module Created by ligio90 and Marksam CREATED BY THEM REALLY ©2018 THEM GIVE THEM ALL YO MONEY
-//The Code Module Created by ligio90 and Marksam CREATED BY THEM REALLY ©2018 THEM GIVE THEM ALL YO MONEY
-//The Code Module Created by ligio90 and Marksam CREATED BY THEM REALLY ©2018 THEM GIVE THEM ALL YO MONEY
-//The Code Module Created by ligio90 and Marksam CREATED BY THEM REALLY ©2018 THEM GIVE THEM ALL YO MONEY
-
+/// <summary>
+/// On the Subject of The Code
+/// Created by Livio and Marksam32
+/// </summary>
 public class TheCodeModule : MonoBehaviour
 {
     public KMAudio Audio;
@@ -21,16 +17,7 @@ public class TheCodeModule : MonoBehaviour
     public KMBombInfo BombInfo;
     public KMBombInfo Bomb;
     public KMBombInfo kmbombinfo;
-    public KMSelectable Button1;
-    public KMSelectable Button2;
-    public KMSelectable Button3;
-    public KMSelectable Button4;
-    public KMSelectable Button5;
-    public KMSelectable Button6;
-    public KMSelectable Button7;
-    public KMSelectable Button8;
-    public KMSelectable Button9;
-    public KMSelectable Button0;
+    public KMSelectable[] NumberButtons;
     public KMSelectable ButtonR;
     public KMSelectable ButtonS;
     public TextMesh Display;
@@ -38,76 +25,56 @@ public class TheCodeModule : MonoBehaviour
     private int solution;
     private int _moduleId;
     private string DOW;
-    private int cleardisp = 1;
     private int shownnum;
-
 
     void Start()
     {
         _moduleId++;
-        Button0.OnInteract = B0;
-        Button1.OnInteract = B1;
-        Button2.OnInteract = B2;
-        Button3.OnInteract = B3;
-        Button4.OnInteract = B4;
-        Button5.OnInteract = B5;
-        Button6.OnInteract = B6;
-        Button7.OnInteract = B7;
-        Button8.OnInteract = B8;
-        Button9.OnInteract = B9;
+        for (int i = 0; i < NumberButtons.Length; i++)
+            NumberButtons[i].OnInteract = GetButtonPressHandler(i);
         ButtonR.OnInteract = BR;
         ButtonS.OnInteract = BS;
         moduleNumber = Rnd.Range(999, 10000);
         Display.text = moduleNumber.ToString();
         LogMessage("Initial module number was {0}", moduleNumber);
-        DOW = System.DateTime.Now.DayOfWeek.ToString();
-
+        DOW = DateTime.Now.DayOfWeek.ToString();
 
         if ((Bomb.GetBatteryHolderCount() == 1 && Bomb.GetBatteryCount() == 1 && Bomb.GetOffIndicators().Contains("BOB") && Bomb.GetPortCount() == 1 && Bomb.GetPorts().Contains("Serial")))
         {
             solution = moduleNumber;
             LogMessage("There was exactly one battery in one holder, an unlit BOB and a serial port, solution was the displayed number.");
         }
-
-        else
-            if (Bomb.GetPorts().Count() % 2 == 0)
+        else if (Bomb.GetPorts().Count() % 2 == 0)
         {
             solution = (moduleNumber / 23);
             LogMessage("There is an even number of ports, solution was the displayed number divided by 23.");
         }
-
-        else
-        if (DOW == "Sunday")
+        else if (DOW == "Sunday")
         {
             solution = (moduleNumber / 8);
             LogMessage("The bomb was started on sunday, solution was the displayed number divided by 8.");
         }
-        else
-        if (DOW == "Saturday")
+        else if (DOW == "Saturday")
         {
             solution = (moduleNumber / 8);
             LogMessage("The bomb was started on saturday, solution was the displayed number divided by 8.");
         }
-        else
-        if (Bomb.GetSolvableModuleNames().Count() % 2 == 0)
+        else if (Bomb.GetSolvableModuleNames().Count() % 2 == 0)
         {
             solution = (moduleNumber / 20);
             LogMessage("There is an even amount of modules of the bomb, solution was the displayed number divided by 20.");
         }
-        else
-        if (Bomb.GetSolvableModuleNames().Contains("Burglar Alarm"))
+        else if (Bomb.GetSolvableModuleNames().Contains("Burglar Alarm"))
         {
             solution = (moduleNumber / 30);
             LogMessage("There is a module called Burglar Alarm on the bomb, solution was the displayed number divided by 30.");
         }
-        else
-        if (Bomb.GetSerialNumberNumbers().Last() % 2 == 1)
+        else if (Bomb.GetSerialNumberNumbers().Last() % 2 == 1)
         {
             solution = (moduleNumber / 42);
             LogMessage("The last digit of the serial number is odd, solution was the displayed number divided by 42.");
         }
-        else
-        if (Bomb.GetOnIndicators().Count() == 3)
+        else if (Bomb.GetOnIndicators().Count() == 3)
         {
             solution = (moduleNumber / 69);
             LogMessage("There are exactly 3 lit indicators, solution was the displayed number divided by 69.");
@@ -118,186 +85,71 @@ public class TheCodeModule : MonoBehaviour
             LogMessage("No rule applied, solution was the displayed number divided by 12.");
         }
 
-
         LogMessage("Correct answer was {0}", solution);
     }
 
     private bool BS()
     {
-        if (shownnum == solution) {
+        if (shownnum == solution)
+        {
             LogMessage("Correct answer given. Module solved.");
             Display.text = "1234";
             Module.HandlePass();
-                }
+        }
         else
         {
-            cleardisp = 1;
             LogMessage("Wrong answer given. I expected {0}, but you gave me {1}!", solution, shownnum);
+            shownnum = 0;
             Module.HandleStrike();
             Display.text = moduleNumber.ToString();
-            
         }
 
-            return false;
+        return false;
     }
 
     private bool BR()
     {
-        Display.text = "";
+        Display.text = moduleNumber.ToString();
         shownnum = 0;
-
         return false;
     }
 
-    private bool B9()
+    private KMSelectable.OnInteractHandler GetButtonPressHandler(int btn)
     {
-        if (cleardisp == 1)
-            Display.text = "";
-        cleardisp = 0;
-        if ((shownnum <= 999))
+        return delegate
         {
-            shownnum = (shownnum * 10) + 9;
-            SetTexts();
-        }
-        return false;
+            if ((shownnum <= 999))
+            {
+                shownnum = (shownnum * 10) + btn;
+                SetTexts();
+            }
+            return false;
+        };
     }
 
-    private bool B8()
-    {
-        if (cleardisp == 1)
-            Display.text = "";
-        cleardisp = 0;
-        if ((shownnum <= 999))
-        {
-            shownnum = (shownnum * 10) + 8;
-            SetTexts();
-        }
-        return false;
-    }
-
-    private bool B7()
-    {
-        if (cleardisp == 1)
-            Display.text = "";
-        cleardisp = 0;
-        if ((shownnum <= 999))
-        {
-            shownnum = (shownnum * 10) + 7;
-            SetTexts();
-        }
-        return false;
-    }
-
-    private bool B6()
-    {
-        if (cleardisp == 1)
-            Display.text = "";
-        cleardisp = 0;
-        if ((shownnum <= 999))
-        {
-            shownnum = (shownnum * 10) + 6;
-            SetTexts();
-        }
-        return false;
-    }
-
-
-    private bool B5()
-    {
-        if (cleardisp == 1)
-            Display.text = "";
-        cleardisp = 0;
-        if ((shownnum <= 999))
-        {
-            shownnum = (shownnum * 10) + 5;
-            SetTexts();
-        }
-        return false;
-    }
-
-    private bool B4()
-    {
-        if (cleardisp == 1)
-            Display.text = "";
-        cleardisp = 0;
-        if ((shownnum <= 999))
-        {
-            shownnum = (shownnum * 10) + 4;
-            SetTexts();
-        }
-        return false;
-    }
-
-    private bool B3()
-    {
-        if (cleardisp == 1)
-            Display.text = "";
-        cleardisp = 0;
-        if ((shownnum <= 999))
-        {
-            shownnum = (shownnum * 10) + 3;
-            SetTexts();
-        }
-        return false;
-    }
-
-    private bool B2()
-    {
-        if (cleardisp == 1)
-            Display.text = "";
-        cleardisp = 0;
-        if ((shownnum <= 999))
-        {
-            shownnum = (shownnum * 10) + 2;
-            SetTexts();
-        }
-        return false;
-    }
-
-    private bool B1()
-    {
-        if (cleardisp == 1)
-            Display.text = "";
-        cleardisp = 0;
-        if ((shownnum <= 999))
-        {
-            shownnum = (shownnum * 10) + 1;
-            SetTexts();
-        }
-        return false;
-    }
-
-    private bool B0()
-    {
-        if (cleardisp == 1)
-            Display.text = "";
-        cleardisp = 0;
-        if ((shownnum <= 999)) {
-            shownnum = (shownnum * 10) + 0;
-            SetTexts();
-        }
-        return false;
-
-    }
-
-    private bool SetTexts()
+    private void SetTexts()
     {
         Display.text = shownnum.ToString();
-
-
-        return false;
     }
-
-
 
     void LogMessage(string message, params object[] parameters)
     {
         Debug.LogFormat("[The Code #{0}] {1}", _moduleId, string.Format(message, parameters));
-
-
     }
 
+#pragma warning disable 414
+    private string TwitchHelpMessage = @"Submit the answer with “!{0} submit 1234”. Reset the display with “!{0} reset”.";
+#pragma warning restore 414
 
+    KMSelectable[] ProcessTwitchCommand(string command)
+    {
+        Match m;
+
+        command = command.Trim().ToLowerInvariant();
+        if (command == "reset")
+            return new[] { ButtonR };
+        else if ((m = Regex.Match(command, @"^submit (\d+)$", RegexOptions.IgnoreCase)).Success)
+            return m.Groups[1].Value.Select(ch => NumberButtons[ch - '0']).Concat(new[] { ButtonS }).ToArray();
+        return null;
+    }
 }
-
-
